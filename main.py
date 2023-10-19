659575829ff43f5e1c041d305bfd8ae1552e05c8
Pedro Leal <pedroleal360@gmail.com>
2023-10-19 10:29:17 -0300

    subir main

--- main.py
+++ main.py
@@ -9,19 +9,24 @@ import os
 
 # Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
+time.sleep(1)
+
+# Obtenha as credenciais da API do HubSpot a partir das variáveis de ambiente
 token = os.getenv('TOKEN')
 auth = os.getenv('AUTH')
 
-# Inicializa o cliente da API do HubSpot
+# Crie uma instância do cliente da API do HubSpot
 api_client = HubSpot(access_token=token)
 
 while True:
-    # Conectar-se ao webhook
+    # Consulta o Webhook
     conn = http.client.HTTPSConnection('api.pipedream.com')
-    conn.request("GET", '/v1/sources/dc_v3u3w74/event_summaries', headers={'Authorization': auth})
+    conn.request("GET", '/v1/sources/dc_v3u3w74/event_summaries', '', {
+        'Authorization': auth,
+    })
     res = conn.getresponse()
 
-    # Verifica o código de status da resposta do webhook
+    # Verifica o código de status da resposta do Webhook
     if res.status == 200:
         # Ler os dados JSON da resposta
         data = res.read()
@@ -30,24 +35,23 @@ while True:
     else:
         print(f"Erro: {res.status} - {res.reason}")
 
-    # Extrair o email e primeiro nome dos dados JSON
-    event_data = json_data.get("data", [])[0].get("event", {}).get("body", {})
-    email = event_data.get("data[email]", "").strip('"')
-    firstname = event_data.get("data[merges][FNAME]", "").strip('"')
+    # Extrai o email e o primeiro nome dos dados do Webhook
+    email = json.dumps(json_data["data"][0]["event"]["body"]["data[email]"]).strip('"')
+    firstname = json.dumps(json_data["data"][0]["event"]["body"]["data[merges][FNAME]"]).strip('"')
 
-    # Tente realizar o cadastro de contato no HubSpot
+    # Tenta criar um novo contato no HubSpot
     try:
-        contact_properties = {"email": email, "firstname": firstname}
-        simple_public_object_input_for_create = SimplePublicObjectInputForCreate(properties=contact_properties)
+        simple_public_object_input_for_create = SimplePublicObjectInputForCreate(
+            properties={"email": email, "firstname": firstname}
+        )
         api_response = api_client.crm.contacts.basic_api.create(
             simple_public_object_input_for_create=simple_public_object_input_for_create
         )
-        print("Adicionado com sucesso:", email, firstname)
+        print("Contato adicionado com sucesso:", email, firstname)
 
-    # Lidar com exceções
+    # Em caso de erro, capture e exiba exceções
     except ApiException as e:
-        print("Exception when creating contact:", e)
-        print("Erro no email e nome:", email, firstname)
-
-    # Aguardar por um segundo antes da próxima iteração
+        print("Erro ao criar o contato: %s\n" % e)
+        print("Erro no email e nome: ", email, firstname)
+        
     time.sleep(1)
